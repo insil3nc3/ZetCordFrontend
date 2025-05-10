3214e7174cf1531392d3640d5adf2a2fd611a1a7
insil3nc3 <minecrafttvem@gmail.com>
2025-05-10 05:48:22 +0200

    file with function to load custom font and common custom screen style sheet

---
+++ screens/utils/screen_style_sheet.py
@@ -0,0 +1,20 @@
+import os
+from PyQt6.QtGui import QFontDatabase, QFont
+
+def load_custom_font(font_size: int = 12) -> QFont | None:
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(cur_dir, "..", "..", "font", "static", "Inter_18pt-Regular.ttf")
+
+    font_id = QFontDatabase.addApplicationFont(font_path)
+    if font_id != -1:
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        return QFont(font_family, font_size)
+    else:
+        print("❌ Ошибка загрузки шрифта:", font_path)
+        return None
+
+screen_style = """
+    background-color: #121212;
+    color: #E8E8E8;
+    font-family: 'Inter', sans-serif;
+"""
