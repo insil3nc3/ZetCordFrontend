3d74f19479cd38aa396e566b9000255d1afaf040
insil3nc3 <minecrafttvem@gmail.com>
2025-05-14 17:55:26 +0200

    open chat with searching user logic added

--- screens/main_screen/search_user.py
+++ screens/main_screen/search_user.py
@@ -2,23 +2,31 @@ import asyncio
 
 from PyQt6.QtCore import Qt, pyqtSlot
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QWidget
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QWidget, QHBoxLayout
 
+from api.private_chat import create_chat
 from api.profile_actions import search_user
 from screens.utils.animate_button import StyledAnimatedButton
 from screens.utils.screen_style_sheet import screen_style, load_custom_font
+from screens.utils.search_screen_profile_widget import SearchScreenProfileWidget
 from screens.utils.widgets import line_edit_style, main_screen_line_edit_style
 from screens.main_screen.search_screen_switcher import AnimatedSwitcher
 
 
 class UserSearchWidget(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, open_chat_callback, insert_item_to_list_callback, focus_to_widget_callback, parent=None, cur_user=None):
         super().__init__(parent)
+        self.cur_user = cur_user
+        self.pos_btn = {"text": "Открыть чат", "connect": "open_chat"}
+        self.un_name = None
+        self.user_widget = None
         self.user_data = None
+        self.open_chat = open_chat_callback
+        self.focus_to_widget = focus_to_widget_callback
+        self.insert_to_list = insert_item_to_list_callback
         self.setWindowTitle("Найти пользователя")
         self.setModal(True)
         self.setFixedSize(550, 300)
-
         self.stack_container = QWidget(self)
         self.stack_container.setGeometry(0, 0, 550, 300)
 
@@ -78,9 +86,22 @@ class UserSearchWidget(QDialog):
     def create_result_screen(self):
         screen = QWidget()
         main_layout = QVBoxLayout()
-        back_button = StyledAnimatedButton("Назад", def_color="#212121")
+        main_layout.addStretch()
+        self.user_widget = SearchScreenProfileWidget()
+        self.user_widget.setFixedWidth(300)
+        main_layout.addWidget(self.user_widget, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        btn_layout = QHBoxLayout()
+        back_button = StyledAnimatedButton("Назад", def_color="#212121", width=200)
         back_button.clicked.connect(lambda: self.switcher.slide_to_index(0, direction="down"))
-        main_layout.addWidget(back_button, alignment=Qt.AlignmentFlag.AlignCenter)
+        btn_layout.addStretch()
+        btn_layout.addWidget(back_button)
+        self.accept_button = StyledAnimatedButton("Начать чат", btn_style="positive", width=200)
+        self.accept_button.clicked.connect(lambda: asyncio.create_task(self.make_positive_action()))
+        btn_layout.addWidget(self.accept_button)
+        btn_layout.addStretch()
+        main_layout.addLayout(btn_layout)
+        main_layout.addStretch()
         screen.setLayout(main_layout)
 
         return screen
@@ -90,4 +111,29 @@ class UserSearchWidget(QDialog):
         unique_name = self.unique_name.text()
         self.user_data = await search_user(unique_name)
         print(self.user_data)
-        self.switcher.slide_to_index(1, direction="up")
+        self.id = self.user_data.get("id")
+        if "request error" in self.user_data:
+            print(self.user_data["request error"])
+            self.alert_label.setText(self.user_data["request error"])
+        else:
+            self.alert_label.setText("")
+            if self.cur_user == self.user_data.get("id"):
+                self.pos_btn = {"text": "Мой профиль", "connect": "open_profile"}
+                self.accept_button.edit_text(self.pos_btn["text"])
+            else:
+                self.pos_btn = {"text": "Открыть чат", "connect": "open_chat"}
+                self.accept_button.edit_text(self.pos_btn["text"])
+            asyncio.create_task(self.user_widget.input_data(self.user_data, unique_name))
+            self.switcher.slide_to_index(1, direction="up")
+
+    async def make_positive_action(self):
+        if self.pos_btn["connect"] == "open_profile":
+            print("открываю профиль")
+        elif self.pos_btn["connect"] == "open_chat":
+            data = await create_chat(self.unique_name.text().strip())
+            print(data)
+            if data["chat_type"] == "new_chat":
+                self.item_widget = self.insert_to_list(self.unique_name.text().strip(), "", self.user_widget.avatar_path, data["_id"], self.id)
+            self.focus_to_widget(self.id)
+            self.open_chat(data["_id"], self.id, self.unique_name.text().strip())
+            self.close()
